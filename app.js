096ba9790c1f27bd2f274b80271756a3ae60bd4a
Sachin Singh <90830959+Sachin79dev@users.noreply.github.com>
2026-06-06 17:54:36 +0530

    Unit calculator for Electricity Bill
    // Unit calculator for Electricity Bill
    // 0-100 units: 4.2 Rs/unit
    // 101-200 units: 6 Rs/unit
    // 201-400 units: 8 Rs/unit
    // Above 400 units: 13 Rs/unit

---
+++ app.js
@@ -0,0 +1,28 @@
+// Unit calculator for Electricity Bill
+// 0-100 units: 4.2 Rs/unit
+// 101-200 units: 6 Rs/unit
+// 201-400 units: 8 Rs/unit
+// Above 400 units: 13 Rs/unit
+
+
+let unit = Number(prompt("Enter the number of units consumed:"));
+let billAmount = 0;
+
+if (unit > 0 && unit <= 100) {
+    billAmount = unit * 4.2;
+    console.log(billAmount);
+}
+else if (unit > 100 && unit <= 200) {
+    billAmount = (100 * 4.2) + ((unit - 100) * 6);
+    console.log(billAmount);
+}
+else if (unit > 200 && unit <= 400) {
+    billAmount = (100 * 4.2) +  (100 * 6) + ((unit - 200) * 8);
+    console.log(billAmount);
+}
+else {
+    billAmount = (100 * 4.2) + (100 * 6) + (200 * 8) + ((unit - 400) * 13);
+    console.log(billAmount);
+}
+
+
