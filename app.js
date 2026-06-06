df7c55b5f17c7e4ba955e2767254934da64ae0c2
Sachin Singh <90830959+Sachin79dev@users.noreply.github.com>
2026-06-06 18:23:56 +0530

    Optimize bill calculation logic in app.js
    Optimize bill calculation logic in app.js

--- app.js
+++ app.js
@@ -26,3 +26,23 @@ else {
 }
 
 
+
+
+// Optimized way for the same 
+
+if(unit>400) {
+    billAmount += (unit - 400) * 13;
+    unit = 400;
+}
+if(unit>200) {
+    billAmount += (unit - 200) * 8;
+    unit = 200;
+}
+if(unit>100) {
+    billAmount += (unit - 100) * 6;
+    unit = 100;
+}
+
+billAmount += unit*4.2;
+
+console.log(billAmount);
